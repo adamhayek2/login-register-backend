fb1fd4efe313f966ea73944a191c8bae58b80439
adamhayek2002 <YOUR_EMAIL>
2023-07-20 22:19:18 +0300

    back-end <> adding signin file

---
+++ signin.php
@@ -0,0 +1,28 @@
+<?php
+    include('connection.php');
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $query = $mysqli->prepare('select id,username,password
+        from users 
+        where username=?');
+    $query->bind_param('s', $username);
+    $query->execute();
+
+    $query->store_result();
+    $query->bind_result($id, $username, $hashed_password, $first_name, $last_name);
+    $query->fetch();
+
+    $num_rows = $query->num_rows();
+    if ($num_rows == 0) {
+        $response['status'] = "user not found";
+    } else {
+        if (password_verify($password, $hashed_password)) {
+            $response['status'] = 'logged in';
+            $response['user_id'] = $id;
+        } else {
+            $response['status'] = "wrong password";
+        }
+    }
+echo json_encode($response);
